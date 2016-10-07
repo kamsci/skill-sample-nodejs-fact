072081ace77be9d48f73fcf8307c4a31152e1327
Krista Calderon <kamsci@gmail.com>
2016-10-06 19:24:01 -0700

    Basic Fun with Facts Skill

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 
-var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-var SKILL_NAME = 'Space Facts';
+var APP_ID = "amzn1.ask.skill.8c6da288-c6e6-4966-a4a5-9e8e50abfd4a"; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]"; Find in "SkillsI Info" section
+var SKILL_NAME = 'Fun with Facts';
 
 /**
  * Array containing space facts.
@@ -20,7 +20,21 @@ var FACTS = [
     "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
     "Saturn radiates two and a half times more energy into space than it receives from the sun.",
     "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "The Moon is moving approximately 3.8 cm away from our planet every year.",
+    "The Honey Badger can be found in Africa, the Middle East and the Indian Subcontinent.",
+    "Honey Badgers have been known to get comfortable in the dens of aardvarks or in the tunnels of foxes, mongooses, or springhares. Really any crevice or hole will do for the honey badger.",
+    "Honey Badgers are so intelligent that they even use tools. Video from Moholoholo Wildlife Rehabilitation Center in South Africa revealed that a team of honey badgers used sticks, a rake, mud, stones, and pure determination in their attempts to escape.",
+    "Carrots used to be purple until the 17th century.",
+    "Which came first, the fruit or the color? Answer, the color. The color orange was named after the fruit.",
+    "Pound cake got its name from its original recipe, which called for a pound each of butter, eggs, sugar, and flour.",
+    "The Parrot fish eats coral and poops sand. This has led to creation for many small islands and beaches of the Caribbean.",
+    "Scientists were monitoring whale stress levels by analyzing their poop and found that their stress plummeted immediately following the 9/11 attacks. It turns out this was due to all air traffic being halted, which calmed the oceans of low frequency noise which whales use to communicate",
+    "The smell of books makes some people need to poop.",
+    "Doctors are transplanting poop from healthy people into the stomachs of people with certain illnesses and it is healing them at a rate of 91-98%.",
+    "Wombats have cube shaped poop, which they use to remember where they live.",
+    "Doctors are transplanting poop from healthy people into the stomachs of people with certain illnesses and it is healing them at a rate of 91-98%.",
+    "Pandas can poop up to 48 pounds per day.",
+    "Dubai has no sewer system. Instead they use poop trucks to haul the entire cities’ excrement away."
 ];
 
 exports.handler = function(event, context, callback) {
@@ -43,12 +57,12 @@ var handlers = {
         var randomFact = FACTS[factIndex];
 
         // Create speech output
-        var speechOutput = "Here's your fact: " + randomFact;
+        var speechOutput = "Fact: " + randomFact;
 
         this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
     },
     'AMAZON.HelpIntent': function () {
-        var speechOutput = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+        var speechOutput = "You can say tell me a fun fact, or, you can say exit... What can I help you with?";
         var reprompt = "What can I help you with?";
         this.emit(':ask', speechOutput, reprompt);
     },
