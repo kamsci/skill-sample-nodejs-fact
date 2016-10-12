fed2deb0529da0be5f5c5f5f137510298f989dcc
Krista Calderon <kamsci@gmail.com>
2016-10-12 09:30:21 -0700

    standard advice app done

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 
-var APP_ID = "amzn1.ask.skill.8c6da288-c6e6-4966-a4a5-9e8e50abfd4a"; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]"; Find in "SkillsI Info" section
-var SKILL_NAME = 'Fun with Facts';
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]"; Find in "Skills Info" section
+var SKILL_NAME = 'advise me';
 
 /**
- * Array containing space facts.
+ * Array containing space advice.
  */
-var FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year.",
-    "The Honey Badger can be found in Africa, the Middle East and the Indian Subcontinent.",
-    "Honey Badgers have been known to get comfortable in the dens of aardvarks or in the tunnels of foxes, mongooses, or springhares. Really any crevice or hole will do for the honey badger.",
-    "Honey Badgers are so intelligent that they even use tools. Video from Moholoholo Wildlife Rehabilitation Center in South Africa revealed that a team of honey badgers used sticks, a rake, mud, stones, and pure determination in their attempts to escape.",
-    "Carrots used to be purple until the 17th century.",
-    "Which came first, the fruit or the color? Answer, the color. The color orange was named after the fruit.",
-    "Pound cake got its name from its original recipe, which called for a pound each of butter, eggs, sugar, and flour.",
-    "The Parrot fish eats coral and poops sand. This has led to creation for many small islands and beaches of the Caribbean.",
-    "Scientists were monitoring whale stress levels by analyzing their poop and found that their stress plummeted immediately following the 9/11 attacks. It turns out this was due to all air traffic being halted, which calmed the oceans of low frequency noise which whales use to communicate",
-    "The smell of books makes some people need to poop.",
-    "Doctors are transplanting poop from healthy people into the stomachs of people with certain illnesses and it is healing them at a rate of 91-98%.",
-    "Wombats have cube shaped poop, which they use to remember where they live.",
-    "Doctors are transplanting poop from healthy people into the stomachs of people with certain illnesses and it is healing them at a rate of 91-98%.",
-    "Pandas can poop up to 48 pounds per day.",
-    "Dubai has no sewer system. Instead they use poop trucks to haul the entire cities’ excrement away."
+var ADVICE = [
+    "Not to spoil the ending, but everything is going to be okay.",
+    "Forget what hurt you in the past, but never forget what it taight you.",
+    "Follow your heart, but take your brain with you.",
+    "Don't complain to others. Write it down. Tell your god. Throw it out. Keep it to yourself.",
+    "Worry less, smile more. Don't regret, just learn and grow",
+    "It's hard to appreciate the value of what you haven't fully invested in.",
+    "Never give up on what you something you really want. It's difficult to wait, but more difficult to regret.",
+    "People don't always need advice. Sometimes all they really need is a hand to hold, an ear to listen, and a heart to understand them.",
+    "You are confined only by the walls you build yourself.",
+    "Don't make a permenant decision from a temporary emotion",
+    "If you want something you've never ahd before, then you have to do something you've never done before.",
+    "When you say yes to others, make sure you are not saying no to yourself.",
+    "Promote what you love instead of bashing what you hate.",
+    "Advice is what we ask for when we already know the answer but wish we didn't.",
+    "You may not be able to decide to be happy, but you sure can decide to be miserable",
+    "Live moves on and you have to let go. If you cling to your past you can't enjoy your future.",
+    "Always be yourself. Unless you can be a unicorn. Then always be a unicorn.",
+    "Do you wonder what everone is thinking about you? They are wondering the same thing.",
+    "You are unique, but your emotions are not. Everyone suffers from the same scared, incesure, or lonely feelings.",
+    "You con't have control over anyones feelings. They con't have control over yours.",
+    "Your imagination is limitless. Manage expectaions carefully.",
+    "Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.",
+    "Don't let what you can't do stop you from doing what you can do.",
+    "Quality is never an accident, it is always the result of intelligent effort.",
+    "Don't get too caught up in praise or critisizm.",
+    "Happiness is found in doing, not merely possessing.",
+    "Attitude is just as important as ability. Have a good one.",
+    "Learn to like yourself and your cruelties and angers will melt away.",
+    "The actions of a person are the best interpretor of their thoughts."
 ];
 
 exports.handler = function(event, context, callback) {
@@ -46,30 +48,30 @@ exports.handler = function(event, context, callback) {
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('GetFact');
+        this.emit('GetAdvice');
     },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
+    'GetAdviceIntent': function () {
+        this.emit('GetAdvice');
     },
-    'GetFact': function () {
-        // Get a random space fact from the space facts list
-        var factIndex = Math.floor(Math.random() * FACTS.length);
-        var randomFact = FACTS[factIndex];
+    'GetAdvice': function () {
+        // Get a random space fact from the space advice list
+        var adviceIndex = Math.floor(Math.random() * ADVICE.length);
+        var randomAdvice = ADVICE[adviceIndex];
 
         // Create speech output
-        var speechOutput = "Fact: " + randomFact;
+        var speechOutput = "Take my advice: " + randomAdvice;
 
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomAdvice)
     },
     'AMAZON.HelpIntent': function () {
-        var speechOutput = "You can say tell me a fun fact, or, you can say exit... What can I help you with?";
+        var speechOutput = "You can say give me advice, or, you can say exit... What can I help you with?";
         var reprompt = "What can I help you with?";
         this.emit(':ask', speechOutput, reprompt);
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', 'Goodbye!');
+        this.emit(':tell', 'Bye Now!');
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', 'Goodbye!');
+        this.emit(':tell', 'Stopping!');
     }
 };
